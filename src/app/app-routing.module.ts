03f1dfa2452aec2bdb05ba657a06cacfd62b39f5
AurelOconnell <aurelienoconnell@gmail.com>
2024-02-15 15:43:32 +0100

    adding dashboard and routing logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent }, //chemin pour aller sur le dashboard, avec url en premier et le composant lié
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, //redirection par défaut sur le dashboard
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
